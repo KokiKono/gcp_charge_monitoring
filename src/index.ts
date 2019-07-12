7aa9f4310b30859bb682c6649ef8f629f4725ed1
kokikono <kk.k.0421@outlook.jp>
2019-07-13 00:00:21 +0900

    add: targetProject isEnableBilling

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
+import {google, cloudbilling_v1} from 'googleapis';
+
+const PROJECT_ID = process.env.TARGET_PID;
+const PROJECT_NAME = `projects/${PROJECT_ID}`;
+
 const chargeMonitoring = (event, callback) => {
     const pubsubMessage = event.data;
     const dataStr = Buffer.from(pubsubMessage.data, 'base64').toString();
@@ -13,5 +18,9 @@ const chargeMonitoring = (event, callback) => {
 
 export const isCostOver = (budge, cost) => {
     return budge <= cost;
+}
+export const isEnableBilling = async (projectName: string, projects: cloudbilling_v1.Resource$Projects) => {
+    const info = await projects.getBillingInfo({name: projectName});
+    return info.data.billingEnabled;
 }
 export default chargeMonitoring;
