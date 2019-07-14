3ebdef0f63230a3fe041da1a8651378967fe258f
KokiKono <kk.k.0421@outlook.jp>
2019-07-14 18:05:56 +0900

    wip: service account permission

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 ## セットアップ
 1. Cloud Pub/Sub APIの有効化
+2. サービスアカウントに課金管理者を設定する。
+   1. 課金管理者の権限は、Google Cloud Platform Console の [お支払い] ページで管理できます。
+   2. https://console.cloud.google.com/billing?_ga=2.47690980.-279294897.1563082332
 
 ##　テストセットアップ
 1. サービスアカウントの設定
+
--- src/test/system.pubsub.test.ts
+++ src/test/system.pubsub.test.ts
@@ -1,15 +1,37 @@
-const childProcess = require(`child_process`);
+import uuid from 'uuid';
+import {PubSub} from '@google-cloud/pubsub';
+import {google} from 'googleapis';
+const childProcess = require('child_process');
 const test = require(`ava`);
-const uuid = require(`uuid`);
-const {PubSub} = require(`@google-cloud/pubsub`);
+
 const pubsub = new PubSub();
 
-const pJson = require('../package.json');
+const pJson = require('../../package.json');
 const function_name = pJson.config.function_name;
 const topicName = pJson.config.pubsub_topic_name;
 const baseCmd = `gcloud beta functions`;
 
+let auth = null;
+
+test.before('gcp auth login',  async t => {
+  auth = await google.auth.getClient({
+    scopes: ['https://www.googleapis.com/auth/cloud-platform']
+  });
+});
+
+test('is disable', async (t) => {
+  const cloudbilling = google.cloudbilling('v1');
+  t.log('auth', auth);
+  // const info = await cloudbilling.billingAccounts.projects.list({name: 'billingAccounts/0131C9-D22C3C-C4A0CD', auth: auth});
+  // info.data.projectBillingInfo.forEach(i => t.log('data', i));
+  const info = await cloudbilling.projects.getBillingInfo({name: 'projects/estimate-dev', auth: auth})
+    .catch(e => t.log('error', e));
+  t.log('info', info);
+});
+
 test(`charge monitoring: should print over amount`, async (t) => {
+  t.pass();
+  return;
   t.plan(1);
   const startTime = new Date(Date.now()).toISOString();
   const data = require('./charge_amount_data').over_data;
