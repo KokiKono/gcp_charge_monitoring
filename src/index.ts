f6e9c00731cd7cc8e8b0dd516d1a377e59a1ef0a
KokiKono <kk.k.0421@outlook.jp>
2019-07-17 18:05:47 +0900

    fix: tslint

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,11 @@ import {google, cloudbilling_v1} from 'googleapis';
 const P_NAME = process.env.P_NAME;
 const PROJECT_NAME = `projects/${P_NAME}`;
 
-export const chargeMonitoring = async (data, context, callback) => {
+export const chargeMonitoring = async (data: any, context: any, callback: any) => {
     const dataStr = Buffer.from(data.data, 'base64').toString();
     const pubsubMessage = JSON.parse(dataStr);
 
-    if (this.isCostOver(pubsubMessage.budgetAmount, pubsubMessage.costAmount)) {
+    if (isCostOver(pubsubMessage.budgetAmount, pubsubMessage.costAmount)) {
         console.log('over amount');
         const cloudbilling = google.cloudbilling('v1');
         const auth = await google.auth.getClient({
@@ -27,20 +27,20 @@ export const chargeMonitoring = async (data, context, callback) => {
     callback();
 }
 
-export const isCostOver = (budge, cost) => {
+export const isCostOver = (budge: number, cost: number) => {
     return budge <= cost;
 }
-export const isEnableBilling = async (projectName: string, projects: cloudbilling_v1.Resource$Projects, auth) => {
+export const isEnableBilling = async (projectName: string, projects: cloudbilling_v1.Resource$Projects, auth: any) => {
     const info = await projects.getBillingInfo({name: projectName, auth: auth});
     return info.data.billingEnabled;
 }
 
-export const disableBilling = async (projectName: string, projects: cloudbilling_v1.Resource$Projects, auth) => {
+export const disableBilling = async (projectName: string, projects: cloudbilling_v1.Resource$Projects, auth: any) => {
     await projects.updateBillingInfo({name: projectName, requestBody: {
         billingEnabled: false
     }, auth: auth});
     // check billing info
-    const is_enableBilling = await this.isEnableBilling(projectName, projects, auth);
+    const is_enableBilling = await isEnableBilling(projectName, projects, auth);
     if (is_enableBilling === false) return true;
     return false;
 }
